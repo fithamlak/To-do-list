576e7309eda1505a00c8e362ba4dc2ac4d7a3134
Fithamlak <justiceofgod01@gmail.com>
2023-01-25 22:40:59 +0300

    Implement Edit  Handler In New File

---
+++ src/modules/editTodo.js
@@ -0,0 +1,33 @@
+import Todo from './todo.js';
+// eslint-disable-next-line import/extensions, import/no-unresolved
+import Delete from '../images/delet-Icon.svg';
+import deleteEventHandler from './deletHandler.js';
+
+const createEditElementForm = (indexTodo) => {
+  const todo = Todo.getTodo(indexTodo);
+  const editElement = document.createElement('form');
+  editElement.setAttribute('action', '#');
+  editElement.setAttribute('method', 'patch');
+  editElement.setAttribute('type', 'submit');
+  editElement.setAttribute('class', 'edit-todo-form');
+  editElement.setAttribute('id', indexTodo);
+  editElement.innerHTML = `
+  <label for="edit-todo" id="edit-todo-label">
+    <input class="checkbox" type="checkbox" />
+    <input
+      type="text"
+      name="edit-todo"
+      class="edit-todo-input"
+      value=${todo.description}
+    />
+  </label>
+  `;
+  const deleteIcon = new Image();
+  deleteIcon.src = Delete;
+  deleteIcon.setAttribute('class', 'icon');
+  deleteIcon.addEventListener('click', deleteEventHandler);
+  editElement.appendChild(deleteIcon);
+  return editElement;
+};
+
+export default createEditElementForm;
