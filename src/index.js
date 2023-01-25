f4b416a0c0808be92c09160d8ca632a431ad100a
Fithamlak <justiceofgod01@gmail.com>
2023-01-25 10:09:40 +0300

    Create an index.js file and set an array of some simple to do tasks

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const taskArray = [
+  {
+    description: 'read webpack',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'read ES6.',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'read React.',
+    completed: false,
+    index: 3,
+  },
+];
