facc2354d02666da20ff91b86371a39b73660eb3
Fithamlak <justiceofgod01@gmail.com>
2023-02-01 21:11:31 +0300

    Finish Add Function both to local storage and the dom, and create _mocks_ for localstorage

--- src/modules/todo.test.js
+++ src/modules/todo.test.js
@@ -1,15 +1,16 @@
 import Todo from './todo.js';
+import { appendToDOM } from './changeDom.js';
 
 const task = new Todo('test', false, 1);
 const taskElement = document.createElement('li');
 document.body.innerHTML = '<div><ul id="list"></ul></div>';
 
-describe('add todo', () => {
-  test('adds a todo to storage', () => {
+describe('add task', () => {
+  test('adds a task to storage', () => {
     expect(Todo.addTodo(task)).toStrictEqual([task]);
   });
-   appendToDOM(taskElement);
-  test('adds a todo to DOM', () => {
+  appendToDOM(taskElement);
+  test('append a task to DOM', () => {
     const list = document.querySelectorAll('#list li');
     expect(list).toHaveLength(1);
   });
