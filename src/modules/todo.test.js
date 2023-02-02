c76e21d96172ae6fd9a8cfece79685083cb3006b
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-02-02 06:01:53 +0000

    update targeted variable

--- src/modules/todo.test.js
+++ src/modules/todo.test.js
@@ -6,17 +6,17 @@ import { appendToDOM, removeFromDOM } from './changeDom.js';
 
 jest.mock('./localStorage.js');
 const task = new Todo('test', false, 1);
-const taskElement = document.createElement('li');
+const newElement = document.createElement('li');
 document.body.innerHTML = '<div><ul id="taskList"></ul></div>';
 // test for add
 describe('add task', () => {
   test('adds a task to storage', () => {
     expect(Todo.addTodo(task)).toStrictEqual([task]);
   });
-  appendToDOM(taskElement);
+  appendToDOM(newElement);
   test('append a task to DOM', () => {
-    const list = document.querySelectorAll('#taskList li');
-    expect(list).toHaveLength(1);
+    const taskList = document.querySelectorAll('#taskList li');
+    expect(taskList).toHaveLength(1);
   });
 });
 // test for delet
@@ -25,8 +25,8 @@ describe('delet task', () => {
     expect(Todo.removeTodo(task.index)).toStrictEqual([]);
   });
   test('deletss a task element in the dom', () => {
-    removeFromDOM(taskElement);
-    const list = document.querySelectorAll('#taskList li');
-    expect(list).toHaveLength(0);
+    removeFromDOM(newElement);
+    const taskList = document.querySelectorAll('#taskList li');
+    expect(taskList).toHaveLength(0);
   });
 });
